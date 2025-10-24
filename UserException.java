21444276b7b10cd7fb54e4a29594e381fb09c6dc
fazlerabbi13 <mohammad0132794@gmail.com>
2025-10-24 16:19:04 +0600

    create my own exceptions by extending the Java Exception class.

--- UserException.java
+++ UserException.java
@@ -5,5 +5,13 @@ class MyException extends Exception{
 }
 
 public class UserException{
-    
+    public static void main(String[] args) {
+        try{
+            throw new MyException("this is custom exception");
+        }
+        catch(MyException e){
+            System.out.println("Caught");
+            System.out.println(e.getMessage());
+        }
+    }
 }
