6d791e24347d82fff0f504766d53d4a6e401a4df
fazlerabbi13 <mohammad0132794@gmail.com>
2025-10-24 15:29:08 +0600

    handle exception array index out of bound.

---
+++ FinallyBlock.java
@@ -0,0 +1,16 @@
+public class FinallyBlock {
+public static void main(String[] args) {
+    int[] number = {1,2,3,4};
+
+    try {
+        System.out.println(number[4]);
+    } catch (ArrayIndexOutOfBoundsException e) {
+        System.out.println("index out of bound");
+    }
+    finally{
+        System.out.println("finally block always executes.");
+    }
+
+    System.out.println("program running");
+}
+}
